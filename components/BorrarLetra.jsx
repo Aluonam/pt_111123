975bc4442202217bdc0a26dd935a68701350f8e3
Paula Cano <paulacanoroman@gmail.com>
2023-11-11 20:48:05 +0100

    feat: funcionalidad input con useState

--- components/BorrarLetra.jsx
+++ components/BorrarLetra.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const BorrarLetra = () => {
+
+    const [fraseUsuario, setFraseUsuario] = useState("aún no ha escrito nada");
+    const [letra, setLetra] = useState("a");
+
 
 
   return (
@@ -8,12 +12,14 @@ const BorrarLetra = () => {
     <h3>Borrar letra de frase que scribe el usuario</h3>
     <br/>
     <h5>Introduzca una frase</h5>
-    <input onChange={(e)=>{e.target.value}}></input>
+    <input onChange={(e)=>{setFraseUsuario(e.target.value)}}></input>
     <br/>
     <h5>Introduzca la letra que desee eliminar</h5>
-    <input onChange={(e)=>{e.target.value}} type="text" required minlength="1" maxlength="1"></input>
+    <input onChange={(e)=>{e.target.value}} type="text" required minlength="1" maxlength="1" ></input>
     <br/>
     <button>eliminar letra</button>
+
+    {fraseUsuario}
     </>
   )
 }
