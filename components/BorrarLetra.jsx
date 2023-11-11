082f51aa5557ff5a40ca1c64befd26f9cedeebd8
Paula Cano <paulacanoroman@gmail.com>
2023-11-11 20:30:59 +0100

    feat: inputs y textos base

--- components/BorrarLetra.jsx
+++ components/BorrarLetra.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const BorrarLetra = () => {
+
+
   return (
-    <div>BorrarLetra</div>
+    <>
+    <h3>Borrar letra de frase que scribe el usuario</h3>
+    <br/>
+    <h5>Introduzca una frase</h5>
+    <input onChange={(e)=>{e.target.value}}></input>
+    <br/>
+    <h5>Introduzca la letra que desee eliminar</h5>
+    <input onChange={(e)=>{e.target.value}} type="text" required minlength="1" maxlength="1"></input>
+    <br/>
+    <button>eliminar letra</button>
+    </>
   )
 }
 
